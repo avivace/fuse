23b18e1ab2eff0d502167aa43cb8281e9aa8d628
Antonio Vivace <puma95.95@gmail.com>
2018-03-02 18:03:29 +0100

    Basic handling of the file upload with Flask

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.6
+
+import os
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+		if 'file' not in request.files:
+			return 400, 'Bad request'
+		if file.filename == '':
+			return 400, 'Bad request'
+		if file:
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return 200, 'Success'
+
+if __name__ == "__main__":
+	app.run(debug=True)

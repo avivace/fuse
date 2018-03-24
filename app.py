a13e6fbe032292a8f0e384925745e52f382ead09
Antonio Vivace <puma95.95@gmail.com>
2018-03-24 19:39:53 +0100

    Gitignore sqlite3 files, fix denied request due to CORS, create subfolder

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ import uuid
 import sqlite3
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
 CORS(app)
+
 app.config.from_object(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
+
 conn = sqlite3.connect('db.sqlite')
 
 @app.route('/upload', methods=['POST'])
@@ -25,10 +27,11 @@ def upload_file():
 			return 'Bad request', 400
 		if file:
 			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			r = {}
-			r['uploadUUID'] = uuid.uuid4().hex[0:6]
-			return jsonify(r)
+			uploadID = uuid.uuid4().hex[0:6]
+			os.makedirs(app.config['UPLOAD_FOLDER']+'/'+ uploadID)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], uploadID, filename))
+			response = jsonify({'uploadUUID': uploadID})
+			return response
 
 if __name__ == "__main__":
 	app.run(debug=True)
